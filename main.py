b21b188a6e477b89fd81ff9b47c7c869b167c3e8
Ni <nizarr1997@gmail.com>
2025-05-15 13:21:42 +0800

    Add datetime, save all reports in 'weather_history.txt', append, and view report history

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import unicodedata
 from weather import get_weather
+from datetime import datetime
 
 def show_menu():
     print("\n--- Weather Forecast ---\n")
     print("1. Check Weather")
-    print("2. Exit")
+    print("2. View Saved Reports")
+    print("3. Exit")
 
 # Main loop
 while True:
@@ -22,10 +24,31 @@ while True:
         print(result)
 
         # Optional: Save to file
-        # with open(f"{city.replace(' ', '_')}_weather.txt", "w", encoding="utf-8") as file:
-        #     file.write(result)
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        filename = "weather_history.txt"
+
+        entry = f"""
+============================
+📅 Time: {now}
+🈁 City: {city}
+{result}
+"""
+
+        # Append to file
+        with open(filename, "a", encoding="utf-8") as file:
+            file.write(entry)
+
+        print(f"✅ Weather for {city} saved to {filename}")
 
     elif choice == "2":
+        try:
+            with open("weather_history.txt", "r", encoding="utf-8") as file:
+                print("\n 📂 Saved Weather Reports:")
+                print(file.read())
+        except FileNotFoundError:
+            print("Sorry, no saved reports found yet.")
+
+    elif choice == "3":
         print("Goodbye. See you later.")
         break
     else:
